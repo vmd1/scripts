cf447c18442ac679b87a39d8fbbc69b52001b0bf
v-mod <80989699+vmd1@users.noreply.github.com>
2025-03-13 09:37:21 +0000

    Create cln.ps1

---
+++ cln.ps1
@@ -0,0 +1,14 @@
+$uninstallKeys = @('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 'HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'); 
+$browsers = @('Opera', 'Firefox', 'Brave', 'DuckDuckGo'); 
+foreach ($key in $uninstallKeys) { 
+    if (Test-Path $key) { 
+        Get-ChildItem -Path $key | ForEach-Object { 
+            $displayName = $_.GetValue('DisplayName'); 
+            $uninstallString = $_.GetValue('UninstallString'); 
+            if ($displayName -and ($browsers -contains $displayName) -and $uninstallString) { 
+                Write-Output "$displayName found. Uninstaller path: $uninstallString"; 
+                Start-Process -FilePath $uninstallString -ArgumentList "/uninstall" -Wait 
+            } 
+        } 
+    } 
+}
