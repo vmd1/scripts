d796432ac41dfd0331f1bdfb63a85ce20fbdd510
v-mod <80989699+vmd1@users.noreply.github.com>
2025-03-14 14:12:24 +0000

    create an unattended script

---
+++ unattended.ps1
@@ -0,0 +1,81 @@
+# Script created by Vivaan Modi
+
+$uninstallKeys = @(
+    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 
+    'HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+
+function Get-InstalledApp($appName) {
+    foreach ($key in $uninstallKeys) {
+        if (Test-Path $key) {
+            Get-ChildItem -Path $key | ForEach-Object {
+                $displayName = $_.GetValue('DisplayName')
+                if ($displayName -and $displayName -match [regex]::Escape($appName)) {
+                    return $_.PSPath
+                }
+            }
+        }
+    }
+    return $null
+}
+
+function Uninstall-App($appName) {
+    $appRegPath = Get-InstalledApp $appName
+    if ($appRegPath) {
+        $uninstallString = (Get-ItemProperty -Path $appRegPath).UninstallString
+        if ($uninstallString) {
+            Write-Output "Uninstalling: $appName"
+            if ($uninstallString -match '^"(.+?)"\s*(.*)') {
+                $exePath = $matches[1]
+                $arguments = $matches[2]
+            } elseif ($uninstallString -match '^(.+?)\s+(.*)') {
+                $exePath = $matches[1]
+                $arguments = $matches[2]
+            } else {
+                $exePath = $uninstallString
+                $arguments = ""
+            }
+            if (Test-Path $exePath) {
+                Start-Process -FilePath $exePath -ArgumentList $arguments -NoNewWindow -Wait
+            } else {
+                Write-Output "Uninstaller not found. Deleting registry entry."
+                Remove-Item -Path $appRegPath -Recurse -Force
+            }
+        }
+    }
+}
+
+function Uninstall-Firefox-Appx {
+    $firefoxAppx = Get-AppxPackage | Where-Object { $_.Name -match "Mozilla.Firefox" }
+    if ($firefoxAppx) {
+        Write-Output "Uninstalling Firefox AppX package: $($firefoxAppx.Name)"
+        Remove-AppxPackage -Package $firefoxAppx.PackageFullName
+    }
+}
+
+function Delete-Browser-Folders {
+    $folders = @(
+        "$env:LOCALAPPDATA\Mozilla",
+        "$env:APPDATA\Mozilla",
+        "$env:ProgramFiles\Opera",
+        "$env:ProgramFiles(x86)\Opera",
+        "$env:LOCALAPPDATA\Opera",
+        "$env:APPDATA\Opera",
+        "$env:LOCALAPPDATA\Opera GX",
+        "$env:APPDATA\Opera GX"
+    )
+    
+    foreach ($folder in $folders) {
+        if (Test-Path $folder) {
+            Remove-Item -Path $folder -Recurse -Force -ErrorAction SilentlyContinue
+            Write-Output "Deleted: $folder"
+        }
+    }
+}
+
+# Execute functions
+Uninstall-App "Firefox"
+Uninstall-Firefox-Appx
+Uninstall-App "Opera"
+Uninstall-App "Opera GX"
+Delete-Browser-Folders
