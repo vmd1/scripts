a5e10f65897001f0564995455d15430a4bced077
Vivaan <hello@848226.xyz>
2025-12-31 13:54:27 +0000

    Refactor installation script for VMD1 to improve readability and structure

--- linux-tools/install-vmd1-sc.sh
+++ linux-tools/install-vmd1-sc.sh
@@ -1,2 +1,10 @@
-echo "curl -sS https://sc.vmd1.dev/l -o /tmp/l > /dev/null 2>&1 && bash /tmp/l \"$@\" && rm /tmp/l" | sudo tee /usr/local/bin/vmd1-sc
+sudo rm -f /usr/local/bin/vmd1-sc
+echo '#!/bin/bash' | sudo tee /usr/local/bin/vmd1-sc > /dev/null
+echo 'curl -sS https://sc.vmd1.dev/l -o /tmp/l > /dev/null 2>&1' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo 'if [ "$#" -eq 0 ]; then' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo '  bash /tmp/l' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo 'else' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo '  bash /tmp/l "$@"' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo 'fi' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
+echo 'rm /tmp/l' | sudo tee -a /usr/local/bin/vmd1-sc > /dev/null
 sudo chmod +x /usr/local/bin/vmd1-sc
